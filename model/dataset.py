24e111308c4e9ee2b9ab6d5d634d7eadbb7b7df5
zephyr-sh <shaynling@gmail.com>
2023-12-05 11:16:55 +0800

    [C] Update dataset

--- model/dataset.py
+++ model/dataset.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, List, Tuple, Union
 import albumentations as A
 import cv2
 import docsaidkit as D
+import docsaidkit.torch as DT
 import numpy as np
 from docsaidkit import INTER
 
@@ -13,10 +14,10 @@ DIR = D.get_curdir(__file__)
 class DefaultImageAug:
 
     def __init__(self, p=0.5):
-        self.coarse_drop_aug = A.CoarseDropout(
+        self.coarse_drop_aug = DT.CoarseDropout(
             max_holes=1, max_height=64, max_width=64, p=p)
         self.aug = A.Compose([
-            A.ShiftScaleRotate(
+            DT.ShiftScaleRotate(
                 shift_limit=0.2,
                 scale_limit=[-0.4, 0.2],
                 border_mode=cv2.BORDER_CONSTANT),
@@ -28,7 +29,7 @@ class DefaultImageAug:
             A.VerticalFlip(),
             A.RandomRotate90(),
             A.Perspective(),
-            A.GaussianBlur(p=1, blur_limit=(7, 11)),
+            A.GaussianBlur(blur_limit=(7, 11), p=0.5),
         ], p=p, keypoint_params=A.KeypointParams(format='xy', remove_invisible=False))
 
     def __call__(self, image: np.ndarray, keypoints: np.ndarray) -> Any:
@@ -74,6 +75,10 @@ class BaseDataset:
         idx = idx % len(self.dataset)
         img_path, poly = self.dataset[idx]
         img = D.imread(img_path)
+
+        if img is None:
+            raise ValueError(f'Image is None: {img_path}')
+
         poly = np.array(poly)
 
         if self.image_size is not None:
@@ -86,17 +91,46 @@ class BaseDataset:
         raise NotImplementedError
 
 
-class MIDVDataset(BaseDataset):
+class MIDV500Dataset(BaseDataset):
 
     def _build(self):
         ds = D.load_json(DIR.parent / 'data' / 'midv_dataset.json')
         dataset = []
         for data in D.Tqdm(ds.values()):
             for d in data:
-                img_path = self.root / d['img_path']
-                gt = D.load_json(self.root / d['gt_path'])['quad']
-                dataset.append((img_path, gt))
+                if D.Path(d['img_path']).parent.parent.parent.parent.stem == 'midv500':
+                    img_path = self.root / d['img_path']
+                    gt = D.load_json(self.root / d['gt_path'])['quad']
+                    dataset.append((img_path, gt))
         return dataset
+
+
+class MIDV2019Dataset(BaseDataset):
+
+    def __init__(self, return_tensor: bool = False, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.return_tensor = return_tensor
+
+    def _build(self):
+        ds = D.load_json(DIR.parent / 'data' / 'midv_dataset.json')
+        dataset = []
+        for data in D.Tqdm(ds.values()):
+            for d in data:
+                if D.Path(d['img_path']).parent.parent.parent.stem == 'midv2019':
+                    img_path = self.root / d['img_path']
+                    gt = D.load_json(self.root / d['gt_path'])['quad']
+                    dataset.append((img_path, gt))
+        return dataset
+
+    def __getitem__(self, idx):
+        if self.return_tensor:
+            img, poly = super().__getitem__(idx)
+            poly = D.Polygon(poly).normalize(
+                w=img.shape[1], h=img.shape[0]).numpy().astype('float32')
+            img = np.transpose(img.astype('float32'), (2, 0, 1)) / 255.0
+            return img, poly
+        else:
+            return super().__getitem__(idx)
 
 
 class CordDataset(BaseDataset):
@@ -116,12 +150,10 @@ class SyncDataset(BaseDataset):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         kwargs.update({'aug_ratio': 0.0})
-        self.midv = MIDVDataset(**kwargs)
+        self.midv = MIDV500Dataset(**kwargs)
         self.cord = CordDataset(**kwargs)
         self.pool = D.get_files(
-            DIR.parent / 'data' / 'docpool',
-            suffix=['.jpg', '.jpeg', '.png']
-        )
+            DIR.parent / 'data' / 'docpool', suffix=['.jpg'])
 
     def _build(self):
         ds = D.load_json(DIR.parent / 'data' / 'indoor_dataset.json')
@@ -132,18 +164,21 @@ class SyncDataset(BaseDataset):
         return dataset
 
     def _random_get_doc_image(self):
-        if np.random.rand() < 0.5:
-            ds = np.random.choice([self.midv, self.cord], p=[0.9, 0.1])
-            return D.imwarp_quadrangle(*ds[np.random.randint(len(ds))])
+        tgt = np.random.choice(['midv', 'cord', 'pool'], p=[0.1, 0.1, 0.8])
+        if tgt == 'cord':
+            return D.imwarp_quadrangle(*self.cord[np.random.randint(len(self.cord))])
+        elif tgt == 'midv':
+            return D.imwarp_quadrangle(*self.midv[np.random.randint(len(self.midv))])
         else:
             img = self.pool[np.random.randint(len(self.pool))]
             return D.imread(img)
 
     def _generate_random_quadrant_points(self):
-        q1_point = np.random.uniform(low=[0.1, 0.1], high=[0.4, 0.4])
-        q2_point = np.random.uniform(low=[0.6, 0.1], high=[0.9, 0.4])
-        q3_point = np.random.uniform(low=[0.6, 0.6], high=[0.9, 0.9])
-        q4_point = np.random.uniform(low=[0.1, 0.6], high=[0.4, 0.9])
+        # 0.3 -> Avoid the error order poins of the image.
+        q1_point = np.random.uniform(low=[0.05, 0.05], high=[0.3, 0.3])
+        q2_point = np.random.uniform(low=[0.6, 0.05], high=[0.95, 0.4])
+        q3_point = np.random.uniform(low=[0.6, 0.6], high=[0.95, 0.95])
+        q4_point = np.random.uniform(low=[0.05, 0.6], high=[0.4, 0.95])
         return np.array([q1_point, q2_point, q3_point, q4_point]).astype('float32')
 
     def _paste_doc_image(self, img, poly, doc_img):
@@ -161,17 +196,24 @@ class SyncDataset(BaseDataset):
         matrix = cv2.getPerspectiveTransform(poly_doc, poly)
         src_warp = cv2.warpPerspective(
             doc_img, matrix, (img.shape[1], img.shape[0]))
-        sync_img = np.where(src_warp > 10, src_warp, img.copy())
+        sync_img = np.where(src_warp > 0, src_warp, img.copy())
 
         return sync_img, poly
 
     def __getitem__(self, idx) -> Tuple[np.ndarray, np.ndarray]:
-        idx = np.random.randint(len(self))
-        img_path, _ = self.dataset[idx]
-        img = D.imread(img_path)
-        if img is None:
-            return self.__getitem__(idx)
-        poly = self._generate_random_quadrant_points()
+
+        if np.random.rand() < 0.5:
+            idx = np.random.randint(len(self))
+            img_path, _ = self.dataset[idx]
+            img = D.imread(img_path)
+            if img is None:
+                return self.__getitem__(idx)
+            poly = self._generate_random_quadrant_points()
+        else:
+            img, poly = self.midv[np.random.randint(len(self.midv))]
+            poly = D.Polygon(poly).normalize(
+                img.shape[1], img.shape[0]).numpy()
+
         doc_img = self._random_get_doc_image()
         sync_img, poly = self._paste_doc_image(img, poly, doc_img)
 
@@ -192,7 +234,7 @@ class DocAlignedDataset:
         aug_func: Callable = None,
         aug_ratio: float = 0.0,
         length_of_dataset: int = 100000,
-        fuse_dataset: List[str] = ['midv', 'cord', 'sync'],
+        fuse_dataset: List[str] = ['midv500', 'cord', 'sync'],
         fuse_ratio: List[float] = [0.4, 0.2, 0.4],
         edge_width: int = 3,
         output_tensor: bool = True,
@@ -211,10 +253,12 @@ class DocAlignedDataset:
 
         dataset = []
         for d in fuse_dataset:
-            if d not in ['midv', 'cord', 'sync']:
+            if d not in ['midv500', 'midv2019', 'cord', 'sync']:
                 raise ValueError(f'Unknown dataset: {d}')
-            if d == 'midv':
-                dataset.append(MIDVDataset(**ds_settings))
+            if d == 'midv500':
+                dataset.append(MIDV500Dataset(**ds_settings))
+            if d == 'midv2019':
+                dataset.append(MIDV2019Dataset(**ds_settings))
             if d == 'cord':
                 dataset.append(CordDataset(**ds_settings))
             if d == 'sync':
@@ -224,24 +268,34 @@ class DocAlignedDataset:
     def __len__(self) -> int:
         return self.length_of_dataset
 
-    def to_tensor(self, img, poly, edge, edge_mask):
+    def to_tensor(self, img, box, poly, edge, edge_mask):
         poly = D.Polygon(poly).normalize(
+            w=img.shape[1], h=img.shape[0]).numpy().astype('float32')
+        box = D.Box(box).normalize(
             w=img.shape[1], h=img.shape[0]).numpy().astype('float32')
         img = np.transpose(img.astype('float32'), (2, 0, 1)) / 255.0
         edge = edge.astype('float32') / 255.0
         edge_mask = edge_mask.astype('float32')
-        return img, poly, edge, edge_mask
+        return img, box, poly, edge, edge_mask
 
     def __getitem__(self, idx) -> Tuple[np.ndarray, np.ndarray]:
         idx = idx % len(self)
         d_idx = np.random.choice(len(self.dataset), p=self.fuse_ratio)
         f_idx = np.random.randint(len(self.dataset[d_idx]))
         img, poly = self.dataset[d_idx][f_idx]
-        edge = cv2.fillPoly(np.zeros_like(
-            img), [poly.astype('int32')], color=(255, 255, 255))
+        edge = cv2.fillPoly(
+            np.zeros_like(img),
+            [poly.astype('int32')],
+            color=(255, 255, 255)
+        )
+        edge = D.imresize(edge, (edge.shape[0] // 2, edge.shape[1] // 2))
         edge = D.imgrandient(D.imbinarize(edge), ksize=self.edge_width)
         edge_mask = D.imdilate(edge, ksize=self.edge_width) > 0
+
+        # Polygon -> Bounding Box
+        box = D.Polygon(poly).to_box(box_mode='XYWH').numpy()
+
         if self.output_tensor:
-            img, poly, edge, edge_mask = self.to_tensor(
-                img, poly, edge, edge_mask)
-        return img, poly, edge, edge_mask
+            return self.to_tensor(img, box, poly, edge, edge_mask)
+
+        return img, box, poly, edge, edge_mask
